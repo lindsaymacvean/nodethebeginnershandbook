3ea3fb3358108dab3b8280857aacb4dc713ea9dd
lindsay macvean <lindsaymacvean@gmail.com>
2018-03-28 18:50:31 +0100

    chapter 4 - creating a module

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var http = require("http");
 
-function onRequest(request, response) {
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("Hello World");
-	response.end();
+function start() {
+	function onRequest(request, response) {
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write("Hello World");
+		response.end();
+	}
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started ");
 }
 
-http.createServer(onRequest).listen(8888);
+exports.start = start;
