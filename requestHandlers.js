3cea331634467f5adca7eca04965a6a932efa242
lindsay macvean <lindsaymacvean@gmail.com>
2018-03-29 08:53:45 +0100

    chapter 6 - adding request handlers

---
+++ requestHandlers.js
@@ -0,0 +1,7 @@
+function start() {
+	console.log("Request handler 'start' was called.");
+}
+
+function upload() {
+	console.log("Request handler 'upload' was called.");
+}
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var http = require("http");
 var url = require("url");
 
-function start(route) {
+function start(route, handle) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
 		response.writeHead(200, {"Content-Type": "text/plain"});
-		route(pathname);
+		route(handle, pathname);
 		response.write("Hello World");
 		response.end();
 	}
