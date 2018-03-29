c1c86d2dd990bd6b7c2beb0ae8d19c9749f289b6
lindsay macvean <lindsaymacvean@gmail.com>
2018-03-29 09:01:01 +0100

    chapter 7 - returning content

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ function start(route, handle) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
 		response.writeHead(200, {"Content-Type": "text/plain"});
-		route(handle, pathname);
-		response.write("Hello World");
+		var content = route(handle, pathname);
+		response.write(content);
 		response.end();
 	}
 	http.createServer(onRequest).listen(8888);
