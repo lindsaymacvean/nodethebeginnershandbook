f0a490510605730fa3dc40a0442ab64189b6f75c
lindsay macvean <lindsaymacvean@gmail.com>
2018-03-30 18:36:34 +0100

    chapter 8 - created blocking action with exec

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,14 @@
+var exec = require("child_process").exec;
+
 function start() {
 	console.log("Request handler 'start' was called.");
+	var content = "empty";
 
-	function sleep(milliSeconds) {
-		var startTime = new Date().getTime();
-		while (true) {
-			var i = new Date().getTime();
-			if (i > startTime + milliSeconds) {
-				return;
-			}
-		}
-	}
-	sleep(10000);
-	return "Hello Start";
+	exec("ls -lah", function (error, stdout, stderr) {
+		content = stdout;
+	});
+
+	return content;
 }
 
 function upload() {
